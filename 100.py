a50e361f32996c8d8d78098cadd53fb51226b4ff
natethegrate1 <natethegrate1@gmail.com>
2015-04-10 00:23:06 -0500

    Update 100.py

--- 100.py
+++ 100.py
@@ -1,14 +1,18 @@
 #this is a test. this is only a test.
+
+prompt = '> '
+
 print "I am a 2 number python calculator. Please enter the first whole number:"
-x = int(raw_input())
+x = int(raw_input(prompt))
 
 print "Now enter the second whole number:"
-y = int(raw_input())
+y = int(raw_input(prompt))
 
-print "Now tell me if you want to \n(1)add\n(2)subtract\n(3)multiply\n(4)divide"
-operand = raw_input()
+print "Now tell me if you want to add, subtract, multiply, divide"
+operand = raw_input(prompt)
 
 def add(x, y):
+	print "Adding"
 	return x + y
 
 def subtract(x, y):
@@ -19,4 +23,17 @@ def multiply(x, y):
 	
 def divide(x, y):
 	return x / y
+
+if operand == "add":
+	solution = add(x, y)
 	
+if operand == "subtract":
+	solution = subract(x, y)
+	
+if operand == "multiply":
+	solution = multiply(x, y)
+	
+if operand == "divide":
+	solution = divide(x, y)
+	
+print solution
